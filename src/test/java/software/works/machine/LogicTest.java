0a605536fcb97966da33adc7261b474e9d521ea8
Xavier Pigeon <xpigeon@wemanity.com>
2016-06-28 22:40:18 +0200

    [Making drinks][refactor] It should make coffee.

--- src/test/java/software/works/machine/LogicTest.java
+++ src/test/java/software/works/machine/LogicTest.java
@@ -4,13 +4,47 @@ import static org.easymock.EasyMock.mock;
 import static org.easymock.EasyMock.replay;
 import static org.easymock.EasyMock.verify;
 
+import java.util.Arrays;
+import java.util.Collection;
+
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameter;
+import org.junit.runners.Parameterized.Parameters;
 
+@RunWith(Parameterized.class)
 public class LogicTest {
 
     private Logic sut;
     private DrinkMaker drinkMakerMock;
+
+    private static class TestProperties {
+        private Drink givenDrink;
+        private Message expectedMessage;
+
+        public TestProperties(Drink givenDrink, String expectedMessage) {
+            this.givenDrink = givenDrink;
+            this.expectedMessage = new Message(expectedMessage);
+        }
+
+        @Override
+        public String toString() {
+            return String.format("Instructions of " + givenDrink + " should be " + expectedMessage);
+        }
+    }
+
+    @Parameter
+    public TestProperties properties;
+
+    @Parameters(name = "{0}")
+    public static Collection<TestProperties> dataSet() {
+        return Arrays.asList(new TestProperties(Drink.TEA, "T::"), //
+                new TestProperties(Drink.CHOCOLATE, "H::"), //
+                new TestProperties(Drink.COFFEE, "C::") //
+        );
+    }
 
     @Before
     public void prepareFixtures() {
@@ -19,38 +53,10 @@ public class LogicTest {
     }
 
     @Test
-    public void should_make_tea() {
+    public void should_make_drink() {
         // GIVEN
-        CustomerOrder customerOrder = new CustomerOrder(Drink.TEA);
-        drinkMakerMock.makeDrink(new Message("T::"));
-        replay(drinkMakerMock);
-
-        // WHEN
-        sut.sendOrderToDrinkMaker(customerOrder);
-
-        // THEN
-        verify(drinkMakerMock);
-    }
-
-    @Test
-    public void should_make_chocolate() {
-        // GIVEN
-        CustomerOrder customerOrder = new CustomerOrder(Drink.CHOCOLATE);
-        drinkMakerMock.makeDrink(new Message("H::"));
-        replay(drinkMakerMock);
-
-        // WHEN
-        sut.sendOrderToDrinkMaker(customerOrder);
-
-        // THEN
-        verify(drinkMakerMock);
-    }
-
-    @Test
-    public void should_make_coffee() {
-        // GIVEN
-        CustomerOrder customerOrder = new CustomerOrder(Drink.COFFEE);
-        drinkMakerMock.makeDrink(new Message("C::"));
+        CustomerOrder customerOrder = new CustomerOrder(properties.givenDrink);
+        drinkMakerMock.makeDrink(properties.expectedMessage);
         replay(drinkMakerMock);
 
         // WHEN
